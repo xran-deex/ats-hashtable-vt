e67534bf2be9fee3a49c9ba7d1f909579110ee2e
Randy Valis <randy.valis@gmail.com>
2021-05-29 10:55:44 -0400

    WIP

--- conanfile.py
+++ conanfile.py
@@ -1,31 +1,13 @@
-from conans import ConanFile, AutoToolsBuildEnvironment
-from conans import tools
-import os
+from atsconan import ATSConan
 
-class ATSConan(ConanFile):
+class ATSConan(ATSConan):
     name = "hashtable-vt"
     version = "0.1"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "make"
-    exports_sources = "*"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": False}
     requires = "linmap-list-vt/0.1@randy.valis/testing"
 
-    def build(self):
-        atools = AutoToolsBuildEnvironment(self)
-        var = atools.vars
-        var['ATSFLAGS'] = "-IATS" + " -IATS ".join([ f"{self.deps_cpp_info[x].build_paths[0]}src" for x in self.deps_cpp_info.deps ])
-        atools.make(vars=var)
-
-    def package(self):
-        self.copy("*.hats", dst="", src="")
-        self.copy("*.dats", dst="", src="")
-        self.copy("*.sats", dst="", src="")
-        if self.options.shared:
-            self.copy("*.so", dst="lib", keep_path=False)
-        else:
-            self.copy("*.a", dst="lib", keep_path=False)
-
     def package_info(self):
+        super().package_info()
         self.cpp_info.libs = ["hashtable-vt"]
