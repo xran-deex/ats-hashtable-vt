837e9c68d48689bc546dfad5506e66101a2d41d5
Randy Valis <randy.valis@gmail.com>
2021-05-29 11:03:24 -0400

    WIP

--- conanfile.py
+++ conanfile.py
@@ -3,9 +3,6 @@ from atsconan import ATSConan
 class ATSConan(ATSConan):
     name = "hashtable-vt"
     version = "0.1"
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": False}
     requires = "linmap-list-vt/0.1@randy.valis/testing"
 
     def package_info(self):
--- tests/conanfile.py
+++ tests/conanfile.py
@@ -3,9 +3,6 @@ from atsconan import ATSConan
 class ATSConan(ATSConan):
     name = "hashtable-vt-tests"
     version = "0.1"
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": False}
     requires = "hashtable-vt/0.1@randy.valis/testing"
     build_requires = "ats-unit-testing/0.1@randy.valis/testing"
 
